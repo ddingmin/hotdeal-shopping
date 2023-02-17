2b0150ea5a602fb6b3edfc505b9336850f7f190c
ddingmin <ddingminyt@gmail.com>
2023-02-17 16:47:50 +0900

    슬랙 알림 연동

--- crawling.py
+++ crawling.py
@@ -1,15 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://www.fmkorea.com/hotdeal'
 
+
+url = 'https://www.fmkorea.com/hotdeal'
+domain = 'https://www.fmkorea.com'
 response = requests.get(url)
 
-if response.status_code == 200:
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    items = soup.select('#bd_1196365581_0 > div > div.fm_best_widget._bd_pc > ul > li > div > h3 > a')
-    for item in items:
-        print(item.get_text().strip())
-else : 
-    print(response.status_code)
+def get_hotdeal_items():
+    # 제목, 주소, 사진
+    items = []
+    links = []
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        datas = soup.select('#bd_1196365581_0 > div > div.fm_best_widget._bd_pc > ul > li > div > h3 > a')
+        for data in datas:
+            items.append(data.get_text(strip = True))
+            links.append(domain + data['href'])
+    else: 
+        print(response.status_code)
+    return items, links
+
+get_hotdeal_items()
